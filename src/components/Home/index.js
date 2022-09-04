312dc9e98260317c8d341942f55fd7fbb0b27e72
12moi <moi.shadrack@student.moringaschool.com>
2022-09-04 21:03:42 +0300

    add updates

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,49 +1,75 @@
-
-import { useEffect } from 'react'
-import { useState } from 'react'
-import LogoTitle from '../../assets/images/logo-s.png'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
 import Logo from './Logo'
 import './index.scss'
-const Home =()=> {
-   const [letterClass, setletterClass]=useState('text-animate')
 
-   const nameArray=['h','a','d','r','a','c','k']
-   const jobArray=['w','e','b','-','d','e','v','e','l','o','p','e','r']
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-   useEffect(()=>{
-    return setTimeout(()=>{
-        setletterClass('text-animate-hover')
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
     }, 4000)
-   },
-   [])
-    return(
-        <div className="container home-page">
-         <div className="text-zone">
-            <h1>
-                
-                <span className={`${letterClass}_12`}>Hi,</span>  
-                <br/> 
-                <span className={`${letterClass}_13`}>I</span> 
-                <span className={`${letterClass}_14`}>'m</span> 
-            <img src={LogoTitle} alt=" developer" />
-           <AnimatedLetters letterClass={letterClass}
-           strArray={nameArray}
-           idx={22}/>
-            <br/>
-            <AnimatedLetters letterClass={letterClass}
-           strArray={jobArray}
-           idx={15}/>
-            </h1>
-            <h2> Frontend Developer / FullStack Developer</h2>
-            <button to="/container" className="flat-button" >CONTACT ME</button>
-            
-         </div>
-         <Logo />
-        </div>
-    )
+  }, [])
 
-    
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Home
