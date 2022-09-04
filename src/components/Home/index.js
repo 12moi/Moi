3292b151afa03128655a0e13a19a0756e24605b8
12moi <moi.shadrack@student.moringaschool.com>
2022-09-04 10:54:13 +0300

    styling the Homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,21 @@
 
+// import { link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
-
+import './index.scss'
 const Home =()=> {
 
     return(
         <div className="container home-page">
          <div className="text-zone">
-            <h1>Hi,  <br/> I'm
+            <h1>Hi,  <br/> I'm  Moi
             <img src={LogoTitle} alt=" developer" />
-            Slobodan
+            hadrack
             <br/>
             Web developer
             </h1>
             <h2> Frontend Developer / FullStack Developer</h2>
-
-            <link to="/container" className="flat-button">CONTACT ME</link>
+            <button to="/container" className="flat-button" >CONTACT ME</button>
+            
          </div>
         </div>
     )
