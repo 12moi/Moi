09a60f3b411f6c43ed265d52c71938ff1564bd3d
12moi <moi.shadrack@student.moringaschool.com>
2022-09-04 13:58:28 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.scss'
-import Home from './components/Home';
+// import Home from './components/Home';
 import Layout from './components/Layout'
+import About from './components/About'
 function App (){
   
     return (
     <>
     <Routes>
       <Route path="/" element={<Layout />} />
-      <Route index element={<Home />} />
+      {/* <Route index element={<Home />} /> */}
+      <Route path='/about' element={<About />} />
     </Routes>
     </>
      
