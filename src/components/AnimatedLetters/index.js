bc9f891f746e23468c6f786a44d6a3be10dae590
12moi <moi.shadrack@student.moringaschool.com>
2022-09-05 12:19:31 +0300

    add changes

--- src/components/AnimatedLetters/index.js
+++ src/components/AnimatedLetters/index.js
@@ -1,18 +1,15 @@
-
 import './index.scss'
 
-const AnimatedLetters = ({LetterClass, strArray, idx}) =>{
-  return(
+const AnimatedLetters = ({ letterClass, strArray, idx }) => {
+  return (
     <span>
-        {
-            strArray.map((char, i)=>(
-                <span key={char +i} className={`${LetterClass}_${i + idx}`}>
-                 {char}
-                </span>
-            ))
-        }
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+          {char}
+        </span>
+      ))}
     </span>
   )
-} ;
+}
 
 export default AnimatedLetters
