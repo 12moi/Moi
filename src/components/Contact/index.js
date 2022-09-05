fcdec1b9f303ca792f8f95dd1cc8c6824691ce05
12moi <moi.shadrack@student.moringaschool.com>
2022-09-05 11:16:45 +0300

    add updates

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -95,10 +95,10 @@ const Contact = () => {
           <span>moishadrack@gmail.com</span>
         </div>
         <div className="map-wrap">
-          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+          <MapContainer center={[-1.30823, 36.73302]} zoom={13}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[44.96366, 19.61045]}>
-              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            <Marker position={[-1.30823, 36.73302]}>
+              <Popup>Shadrack lives here, come over for a cup of coffee :)</Popup>
             </Marker>
           </MapContainer>
         </div>
